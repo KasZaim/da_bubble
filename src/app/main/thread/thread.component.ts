b9eb2aec2c0a6192ff65177d30e8ba6cce17b4df
Ghulammustafa Kakar <145684065+Kakar21@users.noreply.github.com>
2024-03-30 02:24:39 +0100

    added thread design (#10)
    * basic structure
    
    * added fonts, imgs & colors
    
    * added routes
    
    * fixed signup route
    
    * added conversations basics
    
    * fully added conversations nav visually
    
    * added chat
    
    * added message input

--- src/app/main/thread/thread.component.ts
+++ src/app/main/thread/thread.component.ts
@@ -1,12 +1,55 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-thread',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, CommonModule],
   templateUrl: './thread.component.html',
   styleUrl: './thread.component.scss'
 })
 export class ThreadComponent {
+  messages = [
+    {
+      avatar: '4',
+      name: 'Noah Braun',
+      time: '14:25 Uhr',
+      message: 'Welche Version ist aktuell von Angular?',
+      reactions: {
 
+      }
+    },
+    {
+      avatar: '5',
+      name: 'Sofia Müller',
+      time: '14:30 Uhr',
+      message: 'Ich habe die gleiche Frage. Ich habe gegoogelt und es scheint, dass die aktuelle Version Angular 13 ist. Vielleicht weiß Frederik, ob es wahr ist.',
+      reactions: {
+        'nerd': 1
+      }
+    },
+    {
+      avatar: '6',
+      name: 'Frederik Beck',
+      time: '15:06 Uhr',
+      message: 'Ja das ist es.',
+      reactions: {
+        'hands-up': 1,
+        'nerd': 3,
+      }
+    }
+  ];
+
+  objectKeys(obj: object) {
+    return Object.keys(obj);
+  }
+
+  objectValues(obj: object) {
+    return Object.values(obj);
+  }
+
+  objectKeysLength(obj: object | string) {
+    return Object.keys(obj).length;
+  }
 }

13d63342adb0839273d62e5b35a440e771d1eca8
Ghulammustafa Kakar <145684065+Kakar21@users.noreply.github.com>
2024-03-30 15:55:38 +0100

    Sign up design (#11)
    * basic structure
    
    * added fonts, imgs & colors
    
    * added routes
    
    * fixed signup route
    
    * added conversations basics
    
    * fully added conversations nav visually
    
    * added chat
    
    * added message input
    
    * added signin design

--- src/app/start/signup/signup.component.ts
+++ src/app/start/signup/signup.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [ MatButtonModule, MatFormFieldModule, MatInputModule, MatCheckboxModule ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+hide: any;
 
 }

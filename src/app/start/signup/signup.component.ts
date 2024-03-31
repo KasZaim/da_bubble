df011781ad7356fba9b48fa47fae40cdf28193d8
Ghulammustafa Kakar <145684065+Kakar21@users.noreply.github.com>
2024-03-31 18:03:06 +0200

    Avatar design (#13)
    * basic structure
    
    * added fonts, imgs & colors
    
    * added routes
    
    * fixed signup route
    
    * added conversations basics
    
    * fully added conversations nav visually
    
    * added chat
    
    * added message input
    
    * added signin design
    
    * added avatar design

--- src/app/start/signup/signup.component.ts
+++ src/app/start/signup/signup.component.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { AvatarComponent } from '../avatar/avatar.component';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [ MatButtonModule, MatFormFieldModule, MatInputModule, MatCheckboxModule ],
+  imports: [MatButtonModule, MatFormFieldModule, MatInputModule, MatCheckboxModule, AvatarComponent, FormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
-hide: any;
+  hide: any;
+  mailTest = false
+  accountCreated = false;
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.accountCreated = true;
+    }
+  }
 
 }

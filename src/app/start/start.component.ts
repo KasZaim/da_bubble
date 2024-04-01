199763e989ac7e18c31fe3d1006d9f87d8afe750
KasZaim <122385481+KasZaim@users.noreply.github.com>
2024-04-01 21:48:20 +0200

    adding animation

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { Router } from '@angular/router';
+import'animate.css';
 
 @Component({
   selector: 'app-start',
   standalone: true,
-  imports: [LoginComponent,
-            ],
+  imports: [LoginComponent],
   templateUrl: './start.component.html',
-  styleUrl: './start.component.scss'
+  styleUrl: './start.component.scss',
 })
 export class StartComponent {
+
   constructor(public router: Router) {
     
   }

716b01dee591e0122dfb0b01ba05a602e0012b4d
MYaglioglu <135035442+MYaglioglu@users.noreply.github.com>
2024-04-16 18:06:17 +0200

    implement first logic edit Profile

--- src/app/dialog-edit-profile-edit-profile/dialog-edit-profile-edit-profile.component.ts
+++ src/app/dialog-edit-profile-edit-profile/dialog-edit-profile-edit-profile.component.ts
@@ -1,6 +1,6 @@
 import { Dialog, DialogModule, DialogRef } from '@angular/cdk/dialog';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import {
@@ -10,28 +10,74 @@ import {
   MatDialogRef,
 } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
+import { FirestoreService } from '../firestore.service';
+import { UsersList } from '../interfaces/users-list';
+import { User } from '../interfaces/user';
+import { HeaderComponent } from '../main/header/header.component';  
+import { DocumentData, doc, onSnapshot } from '@angular/fire/firestore';
+
 
 @Component({
   selector: 'app-dialog-edit-profile-edit-profile',
   standalone: true,
-  imports: [    
+  imports: [
     CommonModule,
     FormsModule,
     MatInputModule,
     MatButtonModule,
     MatDialogActions,
     MatDialogContent,
-      
+    HeaderComponent,
+
   ],
   templateUrl: './dialog-edit-profile-edit-profile.component.html',
   styleUrl: './dialog-edit-profile-edit-profile.component.scss'
 })
-export class DialogEditProfileEditProfileComponent {
+export class DialogEditProfileEditProfileComponent{
+  
+  currentUserUid: string | null = '';
+  currentUser: UsersList = {
+    id: '',
+    name: '',
+    avatar: '',
+    online: false
+  };
+
   constructor(
     public dialogRef: MatDialogRef<DialogEditProfileEditProfileComponent>,
-    public dialog: MatDialog) {}
+    public dialog: MatDialog,
+    private firestore: FirestoreService) {
+      this.firestore.currentUser$.subscribe(uid => {
+        this.currentUserUid = uid;
+        this.subCurrentUser();
+        // Führen Sie hier Aktionen aus, die vom aktuellen Benutzerstatus abhängen
+      });
+    }
 
-  onNoClick() {
-    this.dialogRef.close();
+    onNoClick() {
+      this.dialogRef.close();
+    }
+
+    subCurrentUser() {
+      let firestore = this.firestore.getFirestore();
+      let ref;
+      if (this.currentUserUid) {
+        ref = doc(firestore, 'users', this.currentUserUid);
+        return onSnapshot(ref, (doc) => {
+          this.currentUser = this.setCurrentUserObj(doc.data(), doc.id);
+        });
+      } else {
+        return console.log('invalid user uid');
+      }
+    }
+  
+    setCurrentUserObj(obj: any, id: string): UsersList {
+      return {
+        id: id || '',
+        name: obj.name || '',
+        avatar: obj.avatar || '',
+        online: obj.online || false
+      }
+    }
+
   }
-}

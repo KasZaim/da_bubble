59226b9b4e109cefd46527746384ebad526522cb
Ghulammustafa Kakar <145684065+Kakar21@users.noreply.github.com>
2024-04-29 22:00:50 +0200

    Avatar feedback (#74)
    * added visual feedback and hover effects

--- src/app/start/avatar/avatar.component.ts
+++ src/app/start/avatar/avatar.component.ts
@@ -1,16 +1,19 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import { SignupComponent } from '../signup/signup.component';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import { NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-avatar',
   standalone: true,
-  imports: [ MatButtonModule, SignupComponent ],
+  imports: [ MatButtonModule, SignupComponent, MatButtonToggleModule ],
   templateUrl: './avatar.component.html',
   styleUrl: './avatar.component.scss'
 })
 export class AvatarComponent {
   avatar = '';
+  error = false;
   @Output() submitAvatar = new EventEmitter<string>();
 
   selectAvatar(number: string) {
@@ -21,6 +24,8 @@ export class AvatarComponent {
     if(this.avatar !== '') {
       console.log(this.avatar)
       this.submitAvatar.emit(this.avatar)
+    } else {
+      this.error = true;
     }
   }
 }
